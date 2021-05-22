cef52a678999cedd6b6dccb9c59ace73c9423d07
Subham Goyal <subham.iitk@gmail.com>
2021-05-23 02:56:12 +0530

    feat: add unique username generator

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,64 @@
-const randomNumber = () => {
-  return Math.floor(100 + Math.random() * 900);
+import * as nouns from "./data/nouns.json";
+import * as adjectives from "./data/adjectives.json";
+
+const randomNumber = (maxNumber: number | undefined) => {
+  switch (maxNumber) {
+    case 1:
+      return Math.floor(Math.random() * 10).toString();
+      break;
+    case 2:
+      return Math.floor(10 + Math.random() * 90).toString();
+      break;
+    case 3:
+      return Math.floor(100 + Math.random() * 900).toString();
+      break;
+    case 4:
+      return Math.floor(1000 + Math.random() * 9000).toString();
+      break;
+    case 5:
+      return Math.floor(10000 + Math.random() * 90000).toString();
+      break;
+    case 6:
+      return Math.floor(100000 + Math.random() * 900000).toString();
+      break;
+    default:
+      return "";
+      break;
+  }
 };
 
-export function generateFromEmail(email: string): string {
+export function generateFromEmail(
+  email: string,
+  randomDigits?: number
+): string {
   // Retrive name from email address
   const nameParts = email.replace(/^(.+)@(.+)$/g, "$1");
   // Replace all special characters like "@ . _";
   const name = nameParts.replace(/[&/\\#,+()$~%._@'":*?<>{}]/g, "");
   // Create unique username
-  const username = name + randomNumber();
+  const username = name + randomNumber(randomDigits);
+  return username;
+}
+
+export function generateUsername(
+  separator?: string,
+  randomDigits?: number,
+  length?: number
+): string {
+  const noun = nouns[Math.floor(Math.random() * nouns.length)];
+  const adjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+
+  let username;
+  // Create unique username
+  if (separator) {
+    username = noun + separator + adjective + randomNumber(randomDigits);
+  } else {
+    username = noun + adjective + randomNumber(randomDigits);
+  }
+
+  if (length) {
+    return username.substring(0, length);
+  }
+
   return username;
 }
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,13 +1,61 @@
-import { generateFromEmail } from "../src/index";
+import { generateFromEmail, generateUsername } from "../src/index";
 import { expect } from "chai";
 
-describe("generate-unique-username unit tests", (): void => {
-  it("generating unique username from email without any special character in the name", (): void => {
+describe("generate-unique-username-from-email unit tests", (): void => {
+  it("generating from email containing no special character in the name", (): void => {
     const actual: string = generateFromEmail("lakshminarayan@example.com");
+    expect(actual).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com");
+    expect(actual).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding one random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 1);
+    expect(actual.slice(0, -1)).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name and adding one random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 1);
+    expect(actual.slice(0, -1)).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding two random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 2);
+    expect(actual.slice(0, -2)).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name and adding two random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 2);
+    expect(actual.slice(0, -2)).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding three random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 3);
     expect(actual.slice(0, -3)).is.equal("lakshminarayan");
   });
-  it("generating unique username from email with a special character in the name", (): void => {
-    const actual: string = generateFromEmail("lakshmi.narayan@example.com");
+  it("generating from email with special character in the name and adding three random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 3);
     expect(actual.slice(0, -3)).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding four random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 4);
+    expect(actual.slice(0, -4)).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name and adding four random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 4);
+    expect(actual.slice(0, -4)).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding five random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 5);
+    expect(actual.slice(0, -5)).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name and adding five random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 5);
+    expect(actual.slice(0, -5)).is.equal("lakshminarayan");
+  });
+  it("generating from email containing no special character in the name and adding six random digit", (): void => {
+    const actual: string = generateFromEmail("lakshminarayan@example.com", 6);
+    expect(actual.slice(0, -6)).is.equal("lakshminarayan");
+  });
+  it("generating from email with special character in the name and adding six random digit", (): void => {
+    const actual: string = generateFromEmail("lakshmi.narayan@example.com", 6);
+    expect(actual.slice(0, -6)).is.equal("lakshminarayan");
   });
 });
