e5dbb51450c8b5d390b07236c2bbc1811e0f4bee
Subham Goyal <subham.iitk@gmail.com>
2021-05-23 04:04:26 +0530

    fix:  pseudorandom number generator is safe using crypto.

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,27 @@
 import * as nouns from "./data/nouns.json";
 import * as adjectives from "./data/adjectives.json";
+import * as crypto from "crypto";
 
 const randomNumber = (maxNumber: number | undefined) => {
   let randomNumberString;
   switch (maxNumber) {
     case 1:
-      randomNumberString = Math.floor(Math.random() * 10).toString();
+      randomNumberString = crypto.randomInt(1, 9).toString();
       break;
     case 2:
-      randomNumberString = Math.floor(10 + Math.random() * 90).toString();
+      randomNumberString = crypto.randomInt(10, 90).toString();
       break;
     case 3:
-      randomNumberString = Math.floor(100 + Math.random() * 900).toString();
+      randomNumberString = crypto.randomInt(100, 900).toString();
       break;
     case 4:
-      randomNumberString = Math.floor(1000 + Math.random() * 9000).toString();
+      randomNumberString = crypto.randomInt(1000, 9000).toString();
       break;
     case 5:
-      randomNumberString = Math.floor(10000 + Math.random() * 90000).toString();
+      randomNumberString = crypto.randomInt(10000, 90000).toString();
       break;
     case 6:
-      randomNumberString = Math.floor(
-        100000 + Math.random() * 900000
-      ).toString();
+      randomNumberString = crypto.randomInt(100000, 900000).toString();
       break;
     default:
       randomNumberString = "";
